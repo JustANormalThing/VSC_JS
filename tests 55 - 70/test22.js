5255504642fbc9db26ace5a5c75a505aef1e137d
TheThing <yagyaev.s.i.22@gmail.com>
2024-01-04 18:41:56 +0300

    test

---
+++ tests 55 - 70/test22.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const schema = mongoose.Schema({name: String});
+
+schema.methods.meow = function(){
+    console.log(this.get("name") + " Said Somthing")
+};
+    
+const Cat = mongoose.model('Ice_', schema);
+
+const kitty = new Cat({name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+kitty.meow();
+//Это для проекта
+//Нужно
