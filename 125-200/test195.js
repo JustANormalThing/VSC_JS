e82385da7da4ed9e0d0af24ce1f72394bb236620
TheThing <yagyaev.s.i.22@gmail.com>
2024-01-06 22:09:13 +0300

    «Test»

---
+++ 125-200/test195.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+arr.splice(3);
+console.log(arr);
+
+let arr1 = [1, 2, 3, 4, 5];
+let newArr = arr1.slice(3);
+console.log(newArr);
---
+++ 125-200/test200.js
@@ -0,0 +1,12 @@
+function func() {
+	console.log('Name');
+}
+
+func();
+
+function func1() {
+	for (let i = 1; i <= 100; i++ )
+	console.log(i);
+}
+
+func1();
