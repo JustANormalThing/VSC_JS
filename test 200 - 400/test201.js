24d6ff4c17512fb895bd466243ba50d76142b9e6
TheThing <yagyaev.s.i.22@gmail.com>
2024-01-06 22:14:42 +0300

    Test

---
+++ test 200 - 400/test201.js
@@ -0,0 +1,16 @@
+function func(num) {
+	console.log(num ** 3);
+}
+
+func(3);
+
+let num1;
+function func1(num1) {
+	if (num1 > 0) {
+		console.log('+++');
+	  } else {
+		console.log('---');
+	  }
+}
+
+func1(3);
